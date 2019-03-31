62d310f2a143d731de11d365cefd7859e8742ff3
James Brumond <james@jbrumond.me>
2019-03-30 19:05:01 -0700

    update healthcheck endpoints

--- src/endpoints/healthcheck.ts
+++ src/endpoints/healthcheck.ts
@@ -1,18 +1,49 @@
 
 import { hostname } from 'os';
 import { server } from '../server';
-import { isShuttingDown } from '@viva-eng/cluster';
+
+interface Healthcheck {
+	status: 'available' | 'dependency failure',
+	hostname: string
+}
+
+interface Dependency {
+	available: boolean,
+	[key: string]: any
+}
+
+interface FullHealthcheck extends Healthcheck {
+	dependencies: {
+		[name: string]: Dependency
+	}
+}
 
 server
 	.get('/healthcheck')
 	.use(({ req, res }) => {
-		const shuttingDown = isShuttingDown();
-		const statusCode = shuttingDown ? 503 : 200;
+		const payload: Healthcheck = {
+			status: 'available',
+			hostname: hostname()
+		};
+
+		res.writeHead(200, { 'content-type': 'application/json' });
+		res.end(JSON.stringify(payload));
+	});
+
+server
+	.get('/healthcheck/full')
+	.use(async ({ req, res }) => {
+		const dependencies: Dependency[] = [ ];
+
+		const available = dependencies.every((dependency) => dependency.available);
+		const statusCode = available ? 200 : 503;
+
+		const payload: FullHealthcheck = {
+			status: available ? 'available' : 'dependency failure',
+			hostname: hostname(),
+			dependencies: { }
+		};
 
 		res.writeHead(statusCode, { 'content-type': 'application/json' });
-		
-		res.end(JSON.stringify({
-			status: shuttingDown ? 'shutting down' : 'available',
-			hostname: hostname()
-		}));
+		res.end(JSON.stringify(payload));
 	});
